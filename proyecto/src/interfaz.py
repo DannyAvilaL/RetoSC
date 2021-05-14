927dd4b5b34dab0f23559241809ddf7045da41b7
DannyAvilaL <a01378664@itesm.mx>
2021-05-14 13:12:22 -0500

    Crea la ventana para el menu de imagenes, se cambia la forma en como se manipulan las ventanas

--- proyecto/src/interfaz.py
+++ proyecto/src/interfaz.py
@@ -10,25 +10,48 @@ Autora: Nancy L. García Jiménez A01378043
 Autora: Daniela Avila Luna      A01378664
 '''
 
-
 import  os
 import sys
+import random as rd
+from datetime import datetime
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
-class MainInterface(Widget):
+#Importacion de clases
+from imagenesinterfaz import ImagenesMenu
+
+class MainInterface(Screen):
     """Clase que se encargará de mostrar el menú principal con las
     aplicaciones disponibles.
     """
-    pass
-        
+    nombre = rd.choice(("DANNY", "NANCY"))
+    horarios = {1: f"BUENOS DÍAS {nombre}", 2: f"BUENAS TARDES {nombre}", 3: f"BUENAS NOCHES {nombre}"}
+    hora = int(datetime.now().strftime("%H"))
+    if(hora > 0 and hora < 12):
+        mensaje = StringProperty(horarios[1])
+    elif(hora >= 12 and hora < 18):
+        mensaje = StringProperty(horarios[2])
+    elif(hora >= 18):
+        mensaje = StringProperty(horarios[3])
+
+    def on_touch_down(self, touch):
+        return super().on_touch_down(touch)
 
 class Interface(App):
 
     def build(self):
-        Window.clearcolor = (1,1,1,1)
-        return MainInterface()
+        Window.clearcolor = (1,1,1,1) #color de fondo
+        sm = ScreenManager()
+        #for pantalla in range(5):
+        #    screen = Screen(name = f"pantalla {pantalla}")
+         #   sm.add_widget(screen)
+        sm.add_widget(MainInterface())
+        sm.add_widget(ImagenesMenu())
+        return sm
+        #return MainInterface()
 
 if __name__ == '__main__':
     Interface().run()
